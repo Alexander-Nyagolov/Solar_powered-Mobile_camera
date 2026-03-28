7daa454b8e9e8d68e46f050036d11521d39cd7a5
Alexander Nyagolov <87063079+Alexander-Nyagolov@users.noreply.github.com>
2026-03-28 16:33:27 +0200

    Add files via upload

---
+++ ESP32_I2S_Camera/ESP32_I2S_Camera.ino
@@ -0,0 +1,166 @@
+#include "OV7670.h"
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include "BMP.h"
+#include <HTTPClient.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+const char* ssid = "Net101(2G)";
+const char* password = "ant3tnt2nt1";
+String hname = "Monitoring-device";
+const char* hostname = hname.c_str();
+
+
+const int SIOD = 21; //SDA
+const int SIOC = 22; //SCL
+
+const int VSYNC = 34;
+const int HREF = 35;
+
+const int XCLK = 32;
+const int PCLK = 33;
+
+const int D0 = 27;
+const int D1 = 17;
+const int D2 = 16;
+const int D3 = 15;
+const int D4 = 14;
+const int D5 = 13;
+const int D6 = 12;
+const int D7 = 4;
+
+OV7670 *camera;
+
+WiFiServer server(80);
+
+unsigned char bmpHeader[BMP::headerSize];
+
+void serve()
+{
+  WiFiClient client = server.available();
+  if (client) 
+  {
+    Serial.println("New Client.");
+    String currentLine = "";
+    while (client.connected()) 
+    {
+      if (client.available()) 
+      {
+        char c = client.read();
+        //Serial.write(c);
+        if (c == '\n') 
+        {
+          if (currentLine.length() == 0) 
+          {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+            client.print(
+              "<style>body{margin: 0}\nimg{height: 100%; width: auto; transform:rotate(90deg)}</style>"
+              "<img id='a' src='/camera' onload='this.style.display=\"initial\"; var b = document.getElementById(\"b\"); b.style.display=\"none\"; b.src=\"camera?\"+Date.now(); '>"
+              "<img id='b' style='display: none' src='/camera' onload='this.style.display=\"initial\"; var a = document.getElementById(\"a\"); a.style.display=\"none\"; a.src=\"camera?\"+Date.now(); '>");
+            client.println();
+            break;
+          } 
+          else 
+          {
+            currentLine = "";
+          }
+        } 
+        else if (c != '\r') 
+        {
+          currentLine += c;
+        }
+        
+        if(currentLine.endsWith("GET /camera"))
+        {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:image/bmp");
+            client.println();
+            
+            client.write(bmpHeader, BMP::headerSize);
+            client.write(camera->frame, camera->xres * camera->yres * 2);
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    //Serial.println("Client Disconnected.");
+  }  
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+  
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  // while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  //   Serial.println("Connection Failed! Rebooting...");
+  //   delay(5000);
+  //   ESP.restart();
+  // }
+
+  // Port defaults to 3232
+  //  ArduinoOTA.setPort(8080);
+
+  // Hostname defaults to esp3232-[MAC]
+  ArduinoOTA.setHostname(hostname);
+   
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  // In setup(), before camera init
+  ledcAttachPin(XCLK, 0); // XCLK pin, channel 0
+  ledcSetup(0, 10000000, 1); // channel 0, 10 MHz, duty cycle resolution 1 bit
+  ledcWrite(0, 1); // Set duty cycle to 50%
+
+  camera = new OV7670(OV7670::Mode::QQVGA_RGB565, SIOD, SIOC, VSYNC, HREF, XCLK, PCLK, D0, D1, D2, D3, D4, D5, D6, D7);
+
+  BMP::construct16BitHeader(bmpHeader, camera->xres, camera->yres);
+
+  server.begin();
+}
+
+void loop()
+{
+  ArduinoOTA.handle();
+  camera->oneFrame();
+  serve();
+}
